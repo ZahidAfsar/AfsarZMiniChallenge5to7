d5284dac73650a5b1c16ee5cd061e399119013ef
Zahid Afsar <zahidtempz@icloud.com>
2024-01-10 10:20:45 -0800

    madlib

---
+++ Services/ChallengeFive/IChallengeFiveService.cs
@@ -0,0 +1,6 @@
+namespace AfsarZMiniChallenge5to7.Services.ChallengeFive;
+
+    public interface IChallengeFiveService
+    {
+        public string MadLib(string nameOfOcean, string diverName, string underwaterRuinName, string nameOfWorld, string whatIsLandInhabitedBy, string whatIsLandInhabitedByTwo, string howDoYouBeAHero, string whatDoYouHate, string whatDoYouLike, string whatMonsterAttackedYou, string weaponUsed);
+    }
